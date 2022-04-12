534672ec655ff79bbf15b2c751d22bf26531d86a
deshayk <deshaykidd@gmail.com>
2022-04-11 22:37:36 -0500

    add program instructions in comments

---
+++ Guessing_Game/main.py
@@ -0,0 +1,10 @@
+import random
+
+#program greeting
+#program asks user to input a number for the range of the random number
+#computer generates a random number between 1 and x (user input)
+#user guesses the number
+#computer tells user if guess is too high or too low
+#if user guess is too high, program prints "Too high. Guess again."
+#if user guess is too low, program prints "Too low. Guess again."
+#if user guess is correct, program prints "You got it! The number was " + number
